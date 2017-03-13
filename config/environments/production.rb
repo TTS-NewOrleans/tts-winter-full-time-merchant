4c98dda11a83ef2d303f8240191ff208918ea9f7
Ryan Hageman <improv42@gmail.com>
2017-03-13 12:05:39 -0500

    Update production and secrets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,24 +11,22 @@ Rails.application.configure do
     }
   }
 
-  config.action_controller.asset_host = ENV['full_domain']
-
-  # Mailers
-  config.action_mailer.asset_host = ENV['full_domain']
-  config.action_mailer.delivery_method = :smtp
+  # Mailer configuration
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               ENV['domain'],
-    user_name:            ENV['gmail_username'],
-    password:             ENV['gmail_password'],
-    authentication:       'plain',
-    enable_starttls_auto: true  }
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: Rails.application.secrets.domain_name,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: Rails.application.secrets.email_provider_username,
+    password: Rails.application.secrets.email_provider_password
+  }
 
-  # config.action_mailer.default_url_options = {
-  #   host: Rails.application.secrets.domain_name
-  # }
-  # config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = {
+    host: Rails.application.secrets.domain_name
+  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
 
   # Code is not reloaded between requests.
