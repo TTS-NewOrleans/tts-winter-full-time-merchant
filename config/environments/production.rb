53fdbd48fdcb12196e82e9f861c310abd00801ba
Ryan Hageman <improv42@gmail.com>
2017-03-13 11:07:17 -0500

    Change environment variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,8 +18,8 @@ Rails.application.configure do
     domain: Rails.application.secrets.domain_name,
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: Rails.application.secrets.gmail_username,
-    password: Rails.application.secrets.gmail_password
+    user_name: Rails.application.secrets.email_provider_username,
+    password: Rails.application.secrets.email_provider_password
   }
 
   config.action_mailer.default_url_options = {
